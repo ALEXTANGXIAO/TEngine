2e7263101a96db4e4298897a0c2c7129d03fdbea
ALEXTANG <574809918@qq.com>
2022-05-23 13:09:28 +0800

    Update TEngineHotUpdate
    Update TEngineHotUpdate

---
+++ TEngineHotUpdate/GameApp.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TEngineHotUpdate
+{
+    public class GameLogicMain
+    {
+        public static void Init()
+        {
+            Debug.Log("Init");
+        }
+
+        public static void Start()
+        {
+            Debug.Log("Start");
+        }
+
+        public static void Update()
+        {
+            Debug.Log("Update");
+        }
+
+        public static void LateUpdate()
+        {
+            Debug.Log("LateUpdate");
+        }
+
+        public static void Destroy()
+        {
+            
+        }
+
+        public static void OnApplicationPause(bool isPause)
+        {
+            
+        }
+    }
+}
