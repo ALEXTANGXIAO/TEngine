8983c52b59739ff4226132e6a144fb1361fbff79
ALEXTANG <574809918@qq.com>
2022-08-26 17:12:43 +0800

    UIModule
    UIModule

--- Assets/TEngine/Scripts/Runtime/UIModule/Scripts/Mgr/UISys.cs
+++ Assets/TEngine/Scripts/Runtime/UIModule/Scripts/Mgr/UISys.cs
@@ -1,20 +1,20 @@
 ﻿namespace TEngine.Runtime.UIModule
 {
-    public partial class UISys:BehaviourSingleton<UISys>
+    public partial class UISys : BehaviourSingleton<UISys>
     {
         public static int DesginWidth => 750;
 
         public static int DesginHeight => 1624;
-      
+
         public static UIManager Mgr
         {
             get { return UIManager.Instance; }
         }
-        
+
         public override void Active()
         {
             base.Active();
-            
+
             RegisterAllController();
         }
 
---
+++ Assets/TEngine/Scripts/Runtime/UIModule/Scripts/UI/DragItem.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace TEngine.Runtime.UIModule
+{
+    public enum UIDragType
+    {
+        Draging,
+        Drop
+    }
+
+    public class DragItem : UIEventItem<DragItem>
+    {
+        private UIDragType m_DragState = UIDragType.Drop;
+        private Vector3 m_ItemOldPos;
+        private Vector3 m_ItemCachePos;
+        private bool m_CanDrag = false;
+
+        /// <summary>
+        /// 是否可以拖拽
+        /// </summary>
+        public bool CanDrag
+        {
+            get { return m_CanDrag; }
+            set
+            {
+                m_CanDrag = value;
+                if (m_CanDrag)
+                {
+                    BindDrag();
+                }
+            }
+        }
+
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            BindDrag();
+        }
+
+        private void BindDrag()
+        {
+            if (m_CanDrag)
+            {
+                BindBeginDragEvent(delegate(DragItem item, PointerEventData data)
+                {
+                    if (!m_CanDrag)
+                    {
+                        return;
+                    }
+
+                    StartDragItem(UIDragType.Draging);
+                });
+
+                BindEndDragEvent(delegate(DragItem item, PointerEventData data)
+                {
+                    if (!m_CanDrag)
+                    {
+                        return;
+                    }
+
+                    EndDrag();
+                });
+            }
+        }
+
+        protected override void OnUpdate()
+        {
+            if (!m_CanDrag)
+            {
+                return;
+            }
+
+            UpdateDragPos();
+        }
+
+        private void StartDragItem(UIDragType type)
+        {
+            if (type != UIDragType.Drop)
+            {
+                m_ItemOldPos = transform.position;
+                Vector3 pos;
+                UISys.Mgr.GetMouseDownUiPos(out pos);
+                m_ItemCachePos = pos;
+                UpdateDragPos();
+                m_DragState = type;
+            }
+        }
+
+        private void EndDrag()
+        {
+            m_DragState = UIDragType.Drop;
+            transform.position = m_ItemOldPos;
+#if UNITY_EDITOR
+            //Debug.LogError("m_ItemCachePos.y - m_ItemOldPos.y " + (m_ItemCachePos.y - m_ItemOldPos.y));
+#endif
+            if (m_ItemCachePos.y - m_ItemOldPos.y > 3)
+            {
+            }
+        }
+
+        private void UpdateDragPos()
+        {
+            if (m_DragState == UIDragType.Drop)
+            {
+                return;
+            }
+
+            Vector3 pos;
+            UISys.Mgr.GetMouseDownUiPos(out pos);
+            transform.position += (pos - m_ItemCachePos);
+            m_ItemCachePos = pos;
+        }
+    }
+}
