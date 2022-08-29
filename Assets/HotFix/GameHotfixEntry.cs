a3fde33dfca5b50c8a65acf447c09cdcd9c659b1
ALEXTANG <574809918@qq.com>
2022-08-29 15:51:51 +0800

    HotFix demo
    HotFix demo

---
+++ Assets/HotFix/GameHotfixEntry.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TEngine.Runtime;
+
+namespace HotFix
+{
+    public class GameHotfixEntry
+    {
+        public static void Start()
+        {
+            Log.Debug("HotFix.GameHotfixEntry");
+        }
+
+        void Update()
+        {
+        
+        }
+    }
+}
+
