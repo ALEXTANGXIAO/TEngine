bdd4a1cff7a4005b6f9e75bc35b611a0aa87bc9a
ALEXTANG <574809918@qq.com>
2022-09-23 16:05:21 +0800

    UIDemo
    UIDemo

---
+++ Assets/TEngine.Demo/TEngine.UIDemo/UITestMain.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TEngine.Runtime;
+using TEngine.Runtime.UIModule;
+using UnityEngine;
+
+public class UITestMain : MonoBehaviour
+{
+    void Start()
+    {
+        //Demo示例，监听TEngine流程加载器OnStartGame事件
+        //抛出这个事件说明框架流程加载完成（热更新，初始化等）
+        GameEventMgr.Instance.AddEventListener(TEngineEvent.OnStartGame,OnStartGame);
+    }
+
+    /// <summary>
+    /// OnStartGame
+    /// </summary>
+    private void OnStartGame()
+    {
+        // 激活UI系统
+        UISys.Instance.Active();
+    }
+}
