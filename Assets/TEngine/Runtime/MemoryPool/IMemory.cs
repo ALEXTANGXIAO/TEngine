75f6c670ac25cd529f04975a3354096648c9868d
ALEXTANG <574809918@qq.com>
2023-03-31 22:39:40 +0800

    Update IMemory.cs

--- Assets/TEngine/Runtime/MemoryPool/IMemory.cs
+++ Assets/TEngine/Runtime/MemoryPool/IMemory.cs
@@ -1,12 +1,12 @@
 ﻿namespace TEngine
 {
     /// <summary>
-    /// 内存对象Interface
+    /// 内存对象Interface。
     /// </summary>
     public interface IMemory
     {
         /// <summary>
-        /// 清理内存对象回收入池
+        /// 清理内存对象回收入池。
         /// </summary>
         void Clear();
     }
