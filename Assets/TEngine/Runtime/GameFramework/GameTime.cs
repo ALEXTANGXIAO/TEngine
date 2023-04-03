cbf145c5cd2e037912bebe79debf4514d3d9d610
ALEXTANG <574809918@qq.com>
2023-04-03 15:46:27 +0800

    Update GameTime.cs

--- Assets/TEngine/Runtime/GameFramework/GameTime.cs
+++ Assets/TEngine/Runtime/GameFramework/GameTime.cs
@@ -1,19 +1,33 @@
-﻿namespace TEngine
+﻿using UnityEngine;
+
+namespace TEngine
 {
-    public class GameTime
+    /// <summary>
+    /// 游戏时间。
+    /// <remarks>统一封装调用。</remarks>
+    /// </summary>
+    public static class GameTime
     {
         public static float time;
 
         public static float deltaTime;
 
+        public static float unscaledDeltaTime;
+
         public static float fixedDeltaTime;
+        
+        public static float frameCount;
         
         public static float unscaledTime;
 
-        public static float unscaledDeltaTime;
-
-        public static float fixedUnscaledDeltaTime;
-
-        public static float realtimeSinceStartup;
+        public static void StartFrame()
+        {
+            time = Time.time;
+            deltaTime = Time.deltaTime;
+            unscaledDeltaTime = Time.unscaledDeltaTime;
+            fixedDeltaTime = Time.fixedDeltaTime;
+            frameCount = Time.frameCount;
+            unscaledTime = Time.unscaledTime;
+        }
     }
 }
