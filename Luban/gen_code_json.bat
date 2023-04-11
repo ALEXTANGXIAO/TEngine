9d70c9d3fd1791417efc956d44d60b3330bbd7d3
ALEXTANG <574809918@qq.com>
2023-04-11 20:11:11 +0800

    Update gen_code_json.bat

--- Luban/gen_code_json.bat
+++ Luban/gen_code_json.bat
@@ -7,7 +7,7 @@ set DATA_OUTPUT=%ROOT_PATH%..\GenerateDatas
 %GEN_CLIENT% -j cfg --^
  -d %CONF_ROOT%\Defines\__root__.xml ^
  --input_data_dir %CONF_ROOT%\Datas ^
- --output_code_dir %WORKSPACE%/Assets/GameMain/Scripts/HotFix/GameProto/GameConfig ^
+ --output_code_dir %WORKSPACE%/Assets/GameScripts/HotFix/GameProto/GameConfig ^
  --output_data_dir ..\GenerateDatas\json ^
  --gen_types code_cs_unity_json,data_json ^
  -s all 
