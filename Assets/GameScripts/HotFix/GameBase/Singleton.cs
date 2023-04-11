6e0785623dcc3b81f3c4e8997ca478f1db3cf002
ALEXTANG <574809918@qq.com>
2023-04-11 19:58:38 +0800

    GameScripts
    GameScripts

---
+++ Assets/GameScripts/HotFix/GameBase/Singleton.cs
@@ -0,0 +1,23 @@
+﻿using TEngine;
+
+namespace GameBase
+{
+    public class Singleton<T> where T:new()
+    {
+        private static T _instance;
+        public static T Instance
+        {
+            get
+            {
+                if (null == _instance)
+                {
+                    _instance = new T();
+                    Log.Assert(_instance != null);
+                }
+
+                return _instance;
+            }
+        }
+
+    }
+}
