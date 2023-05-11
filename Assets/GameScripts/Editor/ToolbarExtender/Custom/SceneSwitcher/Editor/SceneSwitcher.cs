a827e501f284ab9b721847763eb9bfa3744a084c
ALEXTANG <574809918@qq.com>
2023-05-11 23:52:42 +0800

    [+] EditorToolbar
    [+] EditorToolbar

---
+++ Assets/GameScripts/Editor/ToolbarExtender/Custom/SceneSwitcher/Editor/SceneSwitcher.cs
@@ -0,0 +1,84 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+namespace UnityToolbarExtender
+{
+    [InitializeOnLoad]
+    public class SceneSwitchLeftButton
+    {
+        private static readonly string SceneName1 = "main";
+        private static readonly string SceneName2 = "main2";
+
+        static SceneSwitchLeftButton()
+        {
+            ToolbarExtender.LeftToolbarGUI.Add(OnToolbarGUI);
+        }
+
+        static readonly string ButtonStyleName = "Tab middle";
+        static GUIStyle _buttonGuiStyle;
+
+        static void OnToolbarGUI()
+        {
+            _buttonGuiStyle ??= new GUIStyle(ButtonStyleName)
+            {
+                padding = new RectOffset(2, 8, 2, 2),
+                alignment = TextAnchor.MiddleCenter,
+                fontStyle = FontStyle.Bold
+            };
+
+            GUILayout.FlexibleSpace();
+            if (GUILayout.Button(
+                    new GUIContent("Launcher", EditorGUIUtility.FindTexture("PlayButton"), $"Start Scene Launcher"),
+                    _buttonGuiStyle))
+            {
+                SceneHelper.StartScene(SceneName1);
+            }
+        }
+    }
+
+    static class SceneHelper
+    {
+        static string _sceneToOpen;
+
+        public static void StartScene(string sceneName)
+        {
+            if (EditorApplication.isPlaying)
+            {
+                EditorApplication.isPlaying = false;
+            }
+
+            _sceneToOpen = sceneName;
+            EditorApplication.update += OnUpdate;
+        }
+
+        static void OnUpdate()
+        {
+            if (_sceneToOpen == null ||
+                EditorApplication.isPlaying || EditorApplication.isPaused ||
+                EditorApplication.isCompiling || EditorApplication.isPlayingOrWillChangePlaymode)
+            {
+                return;
+            }
+
+            EditorApplication.update -= OnUpdate;
+
+            if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+            {
+                string[] guids = AssetDatabase.FindAssets("t:scene " + _sceneToOpen, null);
+                if (guids.Length == 0)
+                {
+                    Debug.LogWarning("Couldn't find scene file");
+                }
+                else
+                {
+                    string scenePath = AssetDatabase.GUIDToAssetPath(guids[0]);
+                    EditorSceneManager.OpenScene(scenePath);
+                    EditorApplication.isPlaying = true;
+                }
+            }
+
+            _sceneToOpen = null;
+        }
+    }
+}
