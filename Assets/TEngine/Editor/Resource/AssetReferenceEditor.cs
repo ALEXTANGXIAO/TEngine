0ce268aaf6dd2818f9b529df4a7e315d2fa7f04a
ALEXTANG <574809918@qq.com>
2023-05-12 00:35:36 +0800

    AssetRefrenceEditor
    AssetRefrenceEditor

---
+++ Assets/TEngine/Editor/Resource/AssetReferenceEditor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace TEngine.Editor.Resource
+{
+    [CustomEditor(typeof(AssetReference), true)]
+    public class AssetReferenceEditor : UnityEditor.Editor
+    {
+        private AssetReference _target;
+        private void OnEnable()
+        {
+            _target = (AssetReference)target;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            EditorGUILayout.BeginHorizontal();
+ 
+            EditorGUILayout.EndHorizontal();
+        }
+    }
+
+}
