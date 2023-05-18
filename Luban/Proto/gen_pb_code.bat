fe0f9afd6d75f349df915d65fc823674dca8b35c
ALEXTANG <574809918@qq.com>
2023-05-19 00:42:38 +0800

    Update gen_pb_code.bat

--- Luban/Proto/gen_pb_code.bat
+++ Luban/Proto/gen_pb_code.bat
@@ -1,21 +1,21 @@
-@REM @echo off & setlocal enabledelayedexpansion
-@REM cd %~dp0
-@REM echo =================start gen proto code=================
-@REM set pb_path=pb_message
-@REM set out_path=..\..\Assets\GameMain\HotFix\Scripts\Proto
-@REM del /f /s /q %out_path%\*.*
-@REM for /f "delims=" %%i in ('dir /b %pb_path%') do (
-@REM echo ------------%%i start gen
-@REM protoc -I=pb_message --csharp_out=../../Assets/GameMain/HotFix/Scripts/Proto pb_message\%%i
-@REM echo ------------%%i gen success
-@REM )
-@REM echo =================end gen proto code=================
-@REM set cur_path=%~dp0
-@REM set outEventPath=../../Assets\GameMain\HotFix\Scripts\Proto\Definition\Constant
-@REM call ProtobufResolver.exe %cur_path%pb_message %outEventPath%
-@REM echo =================end gen proto event=================
-@REM pause
+@echo off & setlocal enabledelayedexpansion
+cd %~dp0
+echo =================start gen proto code=================
+set pb_path=pb_schemas
+set out_path=..\..\Assets\GameScripts\HotFix\GameProto\GameProtocol
+del /f /s /q %out_path%\*.*
+for /f "delims=" %%i in ('dir /b %pb_path%') do (
+echo ------------%%i start gen
+protoc -I=pb_schemas --csharp_out=../../Assets/GameScripts/HotFix/GameProto/GameProtocol pb_schemas\%%i
+echo ------------%%i gen success
+)
+echo =================end gen proto code=================
+set cur_path=%~dp0
+set outEventPath=../../Assets\GameScripts\HotFix\GameProto\GameProtocol
+call ProtobufResolver.exe %cur_path%pb_schemas %outEventPath%
+echo =================end gen proto event=================
+pause
 
-protoc -I=pb_schemas --csharp_out=Gen pb_schemas\proto_cs.proto proto_cs_common.proto pb_schemas\proto_cs_player.proto
+@REM protoc -I=pb_schemas --csharp_out=Gen pb_schemas\proto_cs.proto proto_cs_common.proto pb_schemas\proto_cs_player.proto
 
 pause
