2258c138922c747e9abc2c73a4147368b8450c26
ALEXTANG <574809918@qq.com>
2023-05-20 11:24:55 +0800

    Create Attribute.cs

---
+++ Assets/GameScripts/HotFix/GameBase/Attribute.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace TEngine
+{
+    #region Attribute
+
+    [AttributeUsage(AttributeTargets.Class)]
+    public class UpdateAttribute : Attribute
+    {
+    }
+
+    [AttributeUsage(AttributeTargets.Class)]
+    public class FixedUpdateAttribute : Attribute
+    {
+    }
+
+    [AttributeUsage(AttributeTargets.Class)]
+    public class LateUpdateAttribute : Attribute
+    {
+    }
+
+    [AttributeUsage(AttributeTargets.Class)]
+    public class RoleLoginAttribute : Attribute
+    {
+    }
+
+    [AttributeUsage(AttributeTargets.Class)]
+    public class RoleLogoutAttribute : Attribute
+    {
+    }
+
+    #endregion
+}
