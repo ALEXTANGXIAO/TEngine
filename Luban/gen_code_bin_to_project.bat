847283939485ed5dc47634c7def78e7df305c2f6
ALEXTANG <574809918@qq.com>
2023-07-10 13:51:32 +0800

    [+] Books

---
+++ Luban/gen_code_bin_to_project.bat
@@ -0,0 +1,17 @@
+set WORKSPACE=..
+
+set GEN_CLIENT=%WORKSPACE%\Luban\Luban.ClientServer\Luban.ClientServer.exe
+set CONF_ROOT=%WORKSPACE%\Luban\Config
+set DATA_OUTPUT=%ROOT_PATH%..\GenerateDatas
+
+%GEN_CLIENT% -j cfg --^
+ -d %CONF_ROOT%\Defines\__root__.xml ^
+ --input_data_dir %CONF_ROOT%\Datas ^
+ --output_code_dir %WORKSPACE%/Assets/GameScripts/HotFix/GameProto/GameConfig ^
+ --output_data_dir %WORKSPACE%/Assets/AssetRaw/Configs/bytes/ ^
+ --gen_types code_cs_unity_bin,data_bin ^
+ -s all 
+
+echo ======== 生成配置文件结束 ========
+
+pause
