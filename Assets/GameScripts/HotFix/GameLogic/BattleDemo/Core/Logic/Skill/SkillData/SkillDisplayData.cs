f5571716a27579924514653199108b66866fdd06
ALEXTANG <574809918@qq.com>
2023-07-17 20:59:13 +0800

    Update SkillDisplayData.cs

--- Assets/GameScripts/HotFix/GameLogic/BattleDemo/Core/Logic/Skill/SkillData/SkillDisplayData.cs
+++ Assets/GameScripts/HotFix/GameLogic/BattleDemo/Core/Logic/Skill/SkillData/SkillDisplayData.cs
@@ -4,7 +4,7 @@ namespace GameLogic.BattleDemo
 {
     /// <summary>
     /// 技能表现数据。
-    /// <remarks>表现数据再由SkillElement组成。</remarks>
+    /// <remarks>表现数据再由SkillElementData组成。</remarks>
     /// </summary>
     [Serializable]
     public class SkillDisplayData
