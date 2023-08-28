278c8f23be2df93d1c19c002502b67d74a243c56
ALEXTANG <574809918@qq.com>
2023-08-28 20:24:31 +0800

    Update DefaultSettingSerializer.cs

--- Assets/TEngine/Runtime/GameFramework/Setting/DefaultSettingSerializer.cs
+++ Assets/TEngine/Runtime/GameFramework/Setting/DefaultSettingSerializer.cs
@@ -5,7 +5,7 @@
     /// </summary>
     public sealed class DefaultSettingSerializer : GameFrameworkSerializer<DefaultSetting>
     {
-        private static readonly byte[] Header = new byte[] { (byte)'G', (byte)'F', (byte)'S' };
+        private static readonly byte[] Header = new byte[] { (byte)'T', (byte)'E', (byte)'S' };
 
         /// <summary>
         /// 初始化默认游戏配置序列化器的新实例。
