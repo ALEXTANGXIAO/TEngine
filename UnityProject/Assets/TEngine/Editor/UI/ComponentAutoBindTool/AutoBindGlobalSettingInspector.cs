381ea8bb8d07fad15fc6738e283445a46e18ce77
ALEXTANG <574809918@qq.com>
2023-10-27 10:48:21 +0800

    增加支持ComponentAutoBindTool自动绑定UI元素组件。
    增加支持ComponentAutoBindTool自动绑定UI元素组件。

---
+++ UnityProject/Assets/TEngine/Editor/UI/ComponentAutoBindTool/AutoBindGlobalSettingInspector.cs
@@ -0,0 +1,12 @@
+﻿using Sirenix.OdinInspector.Editor;
+using UnityEditor;
+
+[CustomEditor(typeof(AutoBindGlobalSetting))]
+public class AutoBindGlobalSettingInspector : OdinEditor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        serializedObject.ApplyModifiedProperties();
+    }
+}
