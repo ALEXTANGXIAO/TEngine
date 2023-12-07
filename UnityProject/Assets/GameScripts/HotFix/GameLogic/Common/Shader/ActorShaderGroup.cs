5d67238c8fbfec2135d1fff3fb09b07b6c29b315
ALEXTANG <574809918@qq.com>
2023-12-07 10:55:50 +0800

    统一封装对shader的管理和示例。
    统一封装对shader的管理和示例。

---
+++ UnityProject/Assets/GameScripts/HotFix/GameLogic/Common/Shader/ActorShaderGroup.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+namespace TEngine
+{
+    /// <summary>
+    /// 封装一个角色可能用到的各种shader场景。
+    /// </summary>
+    class ActorShaderGroup
+    {
+        private readonly TShader[] _allShader = new TShader[(int)ActorShaderEnvType.EnvTypeMax];
+
+        /// <summary>
+        /// 增加Shader到角色Shader分组。
+        /// </summary>
+        /// <param name="shaderType">当前环境类型。</param>
+        /// <param name="shader">TShader。</param>
+        public void AddShader(ActorShaderEnvType shaderType, TShader shader)
+        {
+            _allShader[(int)shaderType] = shader;
+        }
+
+        /// <summary>
+        /// 根据当前环境获取Shader。
+        /// </summary>
+        /// <param name="type">当前环境类型。</param>
+        /// <returns>TShader。</returns>
+        public TShader GetShader(ActorShaderEnvType type)
+        {
+            return _allShader[(int)type];
+        }
+
+        /// <summary>
+        /// 判断是否符合shader集合。
+        /// </summary>
+        /// <param name="shader">Shader实例。</param>
+        /// <returns>是否符合。</returns>
+        public bool IsMatch(Shader shader)
+        {
+            foreach (var dodShader in _allShader)
+            {
+                if (dodShader != null && dodShader.Shader == shader)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ UnityProject/Assets/GameScripts/HotFix/GameLogic/Common/Shader/TShader.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace TEngine
+{
+    /// <summary>
+    /// TShader scripts used for all rendering.
+    /// <remarks>统一封装对shader的管理。</remarks>
+    /// </summary>
+    public class TShader
+    {
+        private bool _loaded;
+        private Shader _shader;
+        private readonly string _shaderName;
+        private readonly string _shaderLocation;
+        private readonly List<string> _keywordOn = new List<string>();
+        private readonly List<string> _keywordOff = new List<string>();
+
+        /// <summary>
+        /// Shader scripts used for all rendering.
+        /// </summary>
+        public Shader Shader
+        {
+            get
+            {
+                if (!_loaded)
+                {
+                    _loaded = true;
+                    _shader = FindShader(_shaderLocation,_shaderName);
+
+                    if (_shader == null)
+                    {
+                        Log.Error($"invalid shader path: {_shaderLocation}, shader name {_shaderName}");
+                    }
+                }
+
+                return _shader;
+            }
+        }
+        
+        /// <summary>
+        /// 查找Shader。
+        /// </summary>
+        /// <param name="shaderLocation">Shader定位地址。</param>
+        /// <param name="shaderName">Shader名称。</param>
+        /// <returns>Shader实例。</returns>
+        public static Shader FindShader(string shaderLocation,string shaderName)
+        {
+            Shader shader = GameModule.Resource.LoadAsset<Shader>(shaderLocation);
+            if (shader != null)
+            {
+                return shader;
+            }
+            return Shader.Find(shaderName);
+        }
+
+        /// <summary>
+        /// TShader构造函数。
+        /// </summary>
+        /// <param name="shaderName">shader名称。</param>
+        /// <param name="shaderLocation">shader路径。</param>
+        public TShader(string shaderName, string shaderLocation)
+        {
+            _shaderName = shaderName;
+            _shaderLocation = shaderLocation;
+            _shader = null;
+        }
+
+        /// <summary>
+        /// TShader构造函数。
+        /// </summary>
+        /// <param name="shaderName">shader名称。</param>
+        /// <param name="shaderLocation">shader路径。</param>
+        /// <param name="keywordOn">开启选项。</param>
+        /// <param name="keywordOff">关闭选项。</param>
+        public TShader(string shaderName, string shaderLocation, string keywordOn, string keywordOff)
+        {
+            _shaderName = shaderName;
+            _shaderLocation = shaderLocation;
+            _shader = null;
+            _keywordOn.Add(keywordOn);
+            _keywordOff.Add(keywordOff);
+        }
+
+        /// <summary>
+        /// TShader构造函数。
+        /// </summary>
+        /// <param name="shaderName">shader名称。</param>
+        /// <param name="shaderLocation">shader路径。</param>
+        /// <param name="keywordOn">开启选项。</param>
+        /// <param name="keywordOff">关闭选项。</param>
+        public TShader(string shaderName, string shaderLocation, string[] keywordOn, string[] keywordOff)
+        {
+            _shaderName = shaderName;
+            _shaderLocation = shaderLocation;
+            _shader = null;
+            _keywordOn.AddRange(keywordOn);
+            _keywordOff.AddRange(keywordOff);
+        }
+
+        /// <summary>
+        /// 设置Shader效果。
+        /// </summary>
+        /// <param name="render">渲染对象。</param>
+        public void ApplyRender(Renderer render)
+        {
+            var sharedMat = render.sharedMaterial;
+            if (sharedMat != null)
+            {
+                //copy一份材质
+                sharedMat = render.material;
+                sharedMat.shader = Shader;
+
+                foreach (var keyword in _keywordOff)
+                {
+                    sharedMat.DisableKeyword(keyword);
+                }
+
+                foreach (var keyword in _keywordOn)
+                {
+                    sharedMat.EnableKeyword(keyword);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 清除shader。
+        /// </summary>
+        /// <param name="render">渲染对象。</param>
+        public void ClearRender(Renderer render)
+        {
+            if (_keywordOff.Count <= 0 && _keywordOn.Count <= 0)
+            {
+                return;
+            }
+
+            var sharedMat = render.sharedMaterial;
+            if (sharedMat != null)
+            {
+                //copy一份材质。
+                sharedMat = render.material;
+                for (int k = 0; k < _keywordOn.Count; k++)
+                {
+                    sharedMat.DisableKeyword(_keywordOn[k]);
+                }
+
+                for (int k = 0; k < _keywordOff.Count; k++)
+                {
+                    sharedMat.EnableKeyword(_keywordOff[k]);
+                }
+            }
+        }
+    }
+}
