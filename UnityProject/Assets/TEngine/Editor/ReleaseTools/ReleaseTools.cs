f016fdd0a6008c373f69cdd3c43aea9d54235313
ALEXTANG <574809918@qq.com>
2024-05-07 20:09:15 +0800

    Update ReleaseTools.cs

--- UnityProject/Assets/TEngine/Editor/ReleaseTools/ReleaseTools.cs
+++ UnityProject/Assets/TEngine/Editor/ReleaseTools/ReleaseTools.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using UnityEditor;
 using UnityEditor.Build.Reporting;
 using UnityEngine;
@@ -225,7 +226,7 @@ namespace TEngine.Editor
 
             BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions
             {
-                scenes = new[] { "Assets/Scenes/main.unity" },
+                scenes = EditorBuildSettings.scenes.Select(scene => scene.path).ToArray(),
                 locationPathName = locationPathName,
                 targetGroup = buildTargetGroup,
                 target = buildTarget,
